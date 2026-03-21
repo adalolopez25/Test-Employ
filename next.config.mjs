c6513b0e17aa90df32de751e43a6828accad3727
kazuto lopez <adaloandres35@gmail.com>
2026-03-21 11:20:42 -0500

    Se elimino la ruta de auth/me/route.ts porque tenia un registro con supabase

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output : 'export',
   images: {
+    // Correcto: 'unoptimized' va aquí afuera
     remotePatterns: [
       {
-        unoptimized :true,
         protocol: 'https',
         hostname: 'rickandmortyapi.com',
         pathname: '/api/character/avatar/**',
       },
     ],
   },
-  basePath : '/adalolopez25.github.io/Rick-And-Morty-APP/'
+  // IMPORTANTE: El basePath solo debe ser el nombre del repositorio
+  // Si tu URL es adalolopez25.github.io/Test-Employ/, el basePath es solo '/Test-Employ'
 };
 
 export default nextConfig;
